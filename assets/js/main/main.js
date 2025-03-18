63da6c9fbfbe64739c98f28deb8955b6cc2ca96f
YCH <dbckgh8010@naver.com>
2025-03-18 19:41:06 +0900

    second commit

--- assets/js/main/main.js
+++ assets/js/main/main.js
@@ -1,34 +1,4 @@
 $(document).ready(function () {
-    function startMarquee($container, reverse) {
-        let speed = 20000;
-        let totalWidth = 0;
-        let $items = $container.find(".scroll-item");
-
-        if ($items.length === 0) return; // 스크롤 아이템이 없으면 실행 안 함
-
-        $items.each((_, el) => totalWidth += $(el).outerWidth(true));
-        $container.append($items.clone()); // 아이템 복제하여 무한 스크롤처럼 만듦
-
-        (function animate() {
-            $container.css("transform", "translateX(0)")
-                .animate(
-                    { transform: `translateX(${reverse ? "" : "-"}${totalWidth}px)` },
-                    {
-                        duration: speed,
-                        easing: "linear",
-                        step: function (now) {
-                            $(this).css("transform", `translateX(${now}px)`);
-                        },
-                        complete: animate // 애니메이션이 끝나면 다시 실행
-                    }
-                );
-        })();
-    };
-
-    $(".scroll-right").each(function () { startMarquee($(this), false); });
-    $(".scroll-left").each(function () { startMarquee($(this), true); });
-
-
     const projectSwiper = new Swiper(".project-swiper", {
         slidesPerView: 3,
         spaceBetween: 30,
@@ -68,7 +38,7 @@ $(document).ready(function () {
         gsap.from($(this), {
             scrollTrigger: {
                 trigger: this,
-                start: "top 95%",
+                start: "top bottom",
                 end: "top 50%",
                 scrub: 2,
             },
@@ -81,8 +51,8 @@ $(document).ready(function () {
         gsap.from($(this), {
             scrollTrigger: {
                 trigger: this,
-                start: "top 95%",
-                end: "top 50%",
+                start: "top bottom",
+                end: "top 80%",
                 scrub: 2,
             },
             opacity: 0,
