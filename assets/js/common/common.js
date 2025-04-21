387ffc418ec43adabdb02b41b52a34c05d0c9baa
YCH <dbckgh8010@naver.com>
2025-04-21 22:27:04 +0900

    header style change

--- assets/js/common/common.js
+++ assets/js/common/common.js
@@ -9,8 +9,9 @@ $(function() {
         var date = dateInfo.getDate();
         var daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
         var dayOfweek = daysOfWeek[dateInfo.getDay()];
-
-        $(".day").html(year + "년&nbsp;" + month + "월&nbsp;" + date + "일&nbsp;" + dayOfweek + "요일");
+        
+        $(".year").html(year + "년&nbsp;")
+        $(".day").html( month + "월&nbsp;" + date + "일&nbsp;" + dayOfweek + "요일");
         $(".time").html(hour + ":" + min + ":" + sec);
     }, 1000);
 
@@ -25,4 +26,76 @@ $(function() {
             escape: "power2.inOut"
         });
     });
+
+    const apiKey = "nbYhWPlb5QvBDwUFGnZfH12pbPlzG3hyq14bgx%2F5B6CahkaMsj1C9QIeG8Q9oxps242SP1n%2FYln%2B5d6YMQr0Mg%3D%3D";
+    const apiUrl = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0";
+    const weatherInner = $(".weather");
+    const error = $(".error");
+    const searchBox = $(".weather-search input");
+    const searchBtn = $(".weather-search button");
+    const weatherIcon = $(".weather-icon");
+    const weatherIconMapping = {
+        Clouds: "clouds",
+        Clear: "clear",
+        Rain: "rain",
+        Drizzle: "drizzle",
+        Mist: "mist",
+        Snow: "snow",
+        Wind: "wind",
+    };
+
+    async function checkWeather(city) {
+        const response = await fetch(
+            apiUrl + encodeURIComponent(city) + `&appid=${apiKey}&lang=kr`
+        );
+
+        if(response.status === 404) {
+            weatherInner.style.display = "none";
+            error.style.display = "block";
+        } else {
+            error.style.display = "none";
+
+            const data = await response.json();
+
+            $(".city").innerHTML = data.name;
+            $(".status").innerHTML = data.weather[0].description;
+            $(".temp").innerHTML = 
+            Math.round(date.main.temp) + "˚C";
+            $(".humidity").innerHTML = data.main.humidity + "%";
+            $(".wind").innerHTML = data.wind.speed + "km/h";
+
+            const mainWeather = data.weather[0].main;
+            if (mainWeather in weatherIconMapping) {
+                weatherIcon.src = `assets/img/common/${weatherIconMapping[mainWeather]}.png`;
+            }
+
+            weatherInner.style.dsiplay = "block";
+        }
+    }
+    weatherInner.style.maxHeight = 0;
+    weatherInner.style.overflow = "hidden";
+    searchBox.on('keypress', function (event) {
+        if (event.key === 'Enter') {
+            checkWeather(searchBox.value);
+            
+            gsap.to(weatherInner, {
+            maxHeight: "100vh",
+            duration: 1,
+            ease: "power2.inOut",
+            });
+        }
+    });
+    
+    searchBtn.on('click', () => {
+        checkWeather(searchBox.value);
+
+        gsap.to(weatherInner, {
+            maxHeight: "100vh",
+            duration: 1,
+            ease: "power2.inOut",
+        });
+    });
+
+    const weatherId = data.weather[0].id;
+    const koreanDescription = weatherDescKo.find((entry) => entry[weatherId]);
 })
